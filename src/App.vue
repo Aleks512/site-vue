f9d8aead6e5237130ea347409a8ba280f692f080
Aleks512 <aleksandracichocka999@gmail.com>
2024-05-03 14:31:16 +0200

    add hide and comuted hiddentasks

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,13 @@
           <h1 class="text-center">Bienvenue dans application Todo de {{ name }} !</h1>
           <p>Ceci est un exemple de contenu pour tester l'affichage de votre application.</p>
           <hr>
-          <div class="d-flex align-items-center justify-content-between px-3 py-2" v-for="t in tasks" :key="t.task">
+          <div class="d-flex align-items-center justify-content-between px-3 py-2" v-for="t in hiddentasks"
+            :key="t.task">
             <div>{{ t.task }}</div>
-            <div><input type="checkbox" v-model="t.completed" class="form-check-input"/>{{ t.completed }}</div>
+            <div><input type="checkbox" v-model="t.completed" class="form-check-input" />{{ t.completed }}</div>
           </div>
+          <input type="checkbox" v-model="hide" class="form-check-input">
+          <label for="">Afficher / Cacher </label>
         </div>
       </div>
     </div>
@@ -23,28 +26,33 @@ export default {
   data() {
     return {
       name: 'Aleks',
-      tasks : [
-    {"task": "Acheter du pain", "completed": false},
-    {"task": "Aller à la gym", "completed": false},
-    {"task": "Envoyer les emails", "completed": false},
-    {"task": "Préparer la réunion de demain", "completed": false},
-    {"task": "Appeler le plombier", "completed": false},
-    {"task": "Planifier les vacances", "completed": false},
-    {"task": "Mettre à jour le CV", "completed": false},
-    {"task": "Réparer la bicyclette", "completed": false},
-    {"task": "Lire le rapport", "completed": false},
-    {"task": "Nettoyer le garage", "completed": false}
-]
-,
+      tasks: [
+        { "task": "Acheter du pain", "completed": false },
+        { "task": "Aller à la gym", "completed": false },
+        { "task": "Envoyer les emails", "completed": false },
+        { "task": "Préparer la réunion de demain", "completed": false },
+        { "task": "Appeler le plombier", "completed": false },
+        { "task": "Planifier les vacances", "completed": false },
+        { "task": "Mettre à jour le CV", "completed": false },
+        { "task": "Réparer la bicyclette", "completed": false },
+        { "task": "Lire le rapport", "completed": false },
+        { "task": "Nettoyer le garage", "completed": false }
+      ],
+      hide: true
     };
   },
-}
+  computed: {
+    hiddentasks() {
+      return this.hide ? this.tasks.filter(t => !t.completed) : this.tasks 
+    }
+  }
+};
 </script>
 
 <style>
- html body {
-    background-color:rgb(247, 208, 208);
-    margin: 0;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-  }
+html body {
+  background-color: rgb(247, 208, 208);
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
 </style>
