9ef8d36dd41a7d4a18c1eeff72625e79bbb019b2
Aleks512 <aleksandracichocka999@gmail.com>
2024-05-07 13:43:02 +0200

    localStorage

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,29 @@
     <div class="container mt-5">
       <div class="row justify-content-md-center">
         <div class="col-md-8 bg-white p-3">
-          <h1 class="text-center">Bienvenue dans l'application Todo de {{ name }} !</h1>
-          <p>Ceci est un exemple de contenu pour tester l'affichage de votre application.</p>
+          <h1 class="text-center">Bienvenue!</h1>
+          <p class="text-center"> Créez votre <span class="badge bg-primary">liste personnalisée</span> de choses à
+            faire</p>
           <hr>
 
-          <!-- Modifications -->
+          <!-- Button Ajouter-->
           <div class="d-flex align-items-center py-2">
-            <input v-model="newItemText" class="form-control flex-grow-1">
+            <input v-model="newItemText" class="borders form-control flex-grow-1">
             <button class="btn btn-primary ms-2" v-on:click="addNewToDO">Ajouter</button>
           </div>
 
+          <div class="d-flex justify-content-end py-2">
+            <label class="form-check ms-2 small">Afficher / Masquer les tâches terminées</label>
+            <input type="checkbox" v-model="hide" class="borders form-check-input ms-2">
+          </div>
+
           <hr>
-          <div class="d-flex align-items-center justify-content-between px-3 py-2" v-for="t in hiddentasks" :key="t.task">
+          <div class="d-flex align-items-center justify-content-between  py-1" v-for="t in hiddentasks" :key="t.task">
             <div>{{ t.task }}</div>
             <div>
-              <input type="checkbox" v-model="t.completed" class="form-check-input" />
-              {{ t.completed }}
+              <input type="checkbox" v-model="t.completed" class="borders form-check-input" />
+              
             </div>
-            
-          </div>
-          <div class="d-flex flex-row-reverse justify-content-between">
-            
-            <input type="checkbox" v-model="hide" class="form-check-input">
-            <label><span class="badge bg-primary">Afficher/Cacher les tasks accomplis</span> </label>
-
 
           </div>
 
@@ -43,24 +42,29 @@ export default {
   data() {
     return {
       name: 'Aleks',
-      tasks: [
-        { "task": "Acheter du pain", "completed": false },
-        { "task": "Aller à la gym", "completed": false },
-        { "task": "Envoyer les emails", "completed": false },
-        { "task": "Préparer la réunion de demain", "completed": false },
-        { "task": "Appeler le plombier", "completed": false },
-        { "task": "Planifier les vacances", "completed": false },
-        { "task": "Mettre à jour le CV", "completed": false },
-        { "task": "Réparer la bicyclette", "completed": false },
-        { "task": "Lire le rapport", "completed": false },
-        { "task": "Nettoyer le garage", "completed": false }
-      ],
-      hide: true
+      tasks: [],
+      hide: true,
+      newItemText: ''
     };
   },
   computed: {
     hiddentasks() {
-      return this.hide ? this.tasks.filter(t => !t.completed) : this.tasks
+      return this.hide ? this.tasks.filter(t => !t.completed) : this.tasks;
+    }
+  },
+  methods: {
+    addNewToDO() {
+      if (this.newItemText.trim()) {
+        this.tasks.push({ task: this.newItemText, completed: false });
+        localStorage.setItem('tasksList', JSON.stringify(this.tasks));
+        this.newItemText = '';
+      }
+    }
+  },
+  created() {
+    let data = localStorage.getItem('tasksList');
+    if (data) {
+      this.tasks = JSON.parse(data);
     }
   }
 };
@@ -68,8 +72,18 @@ export default {
 
 <style>
 html body {
-  background-color: rgb(247, 208, 208);
+  background-color: rgb(12, 85, 231);
+
   margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  color: rgb(12, 85, 231) !important;
+}
+
+.borders {
+  border: 0.2px solid rgb(12, 85, 231) !important;
+  border-radius: 5px !important;
 }
 </style>
